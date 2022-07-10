d72031169341156e40b1db5c4f3a5d4a647726a0
DawidekZagajnik <krukdawid27@gmail.com>
2022-07-10 03:33:11 +0200

    basic review loading setup

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -5,7 +5,7 @@ import useAutoLoad from "../utils/useAutoLoad";
 import "./Restaurant.css";
 import apiCall from "../utils/apiCall";
 import ErrorBox from "../components/ErrorBox";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Typography } from "@mui/material";
 
 
 export default function Restaurant() {
@@ -16,13 +16,18 @@ export default function Restaurant() {
     const [restaurant, setRestaurant] = React.useState(null);
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(null);
-    //const {} = useAutoLoad({url: `/reviews/${restaurantId}`, pageSize: 10, componentMounted: mounted });
+    const {observer, loading: reviewsLoading, error: reviewsError, items: reviews, hasMore} = useAutoLoad({
+        url: `/reviews/${restaurantId}`, 
+        pageSize: 10, 
+        componentMounted: mounted 
+    });
 
     React.useEffect(() => {
         mounted.current = true;
 
         setError(null);
         setLoading(true);
+        setRestaurant(null);
         apiCall({
             url: `/restaurant/${restaurantId}`,
             method: "GET"
@@ -44,12 +49,38 @@ export default function Restaurant() {
         return <Navigate to={"/"}/>;
     }
 
+    const Review = ({index, content, rate, timestamp, user}) => {
+
+        React.useEffect(() => {
+            if (!reviewsLoading && hasMore && index === reviews.length - 1) {
+                observer.observe(document.getElementById("review" + index))
+            }
+        }, [index])
+
+        return <div className="restaurant-review" id={"review" + index}>
+            <Typography variant="h6">{user}</Typography>
+            <Typography variant="p" className="review-date">{timestamp}</Typography>
+        </div>
+    }
+
     return <>
         <NavigationBar />
         <ErrorBox errorMessage={error}/>
         {loading && <div style={{width: "100%", minHeight: "300px", display: "flex", alignItems: "center", justifyContent: "center"}}><CircularProgress /></div>}
-        <div className="restaurant-details-page">
-            Hello
-        </div>
+        {restaurant !== null &&
+            <div className="restaurant-details-page">
+                <Typography variant="h2" style={{marginBottom: "20px"}}>{restaurant.name}</Typography>
+                <Typography className="owner-name" variant="h6">Owner: {restaurant.owner}</Typography>
+                <Typography className="restaurant-description" variant="h4">{restaurant.description}</Typography>
+                <Typography className="restaurant-reviews" variant="h3" color="primary">Reviews</Typography>
+                {reviews.length > 0 ?
+                    reviews.map((review, index) => <Review key={index} index={index} {...review}/>)
+                        :
+                    <Typography variant="h6">This restaurant has no reviews yet</Typography>
+                }
+                <ErrorBox errorMessage={reviewsError}/>
+                {reviewsLoading && <CircularProgress />}
+            </div>
+        }
     </>;
 }
